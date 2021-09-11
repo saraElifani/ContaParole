f9b27ab3bf9994b02f5cccac5bda567c3d9735c9
saradeveloper92 <sara.elifani92@gmail.com>
2021-09-11 09:51:29 +0200

    implementazione algoritmo

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,41 @@
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Main {
+    public static Map<String,Integer> contaParole = new HashMap<>();
 
     public static void main(String[] args){
-
+        String input = JOptionPane.showInputDialog(null, "Inserisci la frase");
+        if(input == null) return;
+        String[] fraseInIngresso = splitFraseInParole(input);
+        for (int i=0; i<fraseInIngresso.length; i++){
+            add(contaParole,fraseInIngresso[i]);
+        }
+        stampaMappa();
     }
+
+    public static String[] splitFraseInParole(String input) {
+        String[] arrayParole = input.split(" ");
+        return arrayParole;
+    }
+
+    //conta degli elementi
+    public static void add(Map<String,Integer> mappa, String parola){
+        Integer nParole = mappa.get(parola);
+        if(nParole==null){
+            mappa.put(parola,1);
+        } else{
+            mappa.put(parola,nParole+1);
+        }
+    }
+
+    //stampa risultati
+    public static void stampaMappa(){
+        for (String parola : contaParole.keySet()) {
+            System.out.println(" - <" + parola + "> ======> e' presente: " + contaParole.get(parola) + " volte.");
+        }
+    }
+
 }
